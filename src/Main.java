cca80041eb2fd620dd662a1d7ec14904589be0aa
Ирина <mi.irene97@gmail.com>
2022-10-01 21:39:41 +0300

    + доп. задачи

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1");
@@ -15,5 +17,50 @@ public class Main {
         fullName = "Иванов Семён Семёнович";
         fullName = fullName.replace('ё', 'е');
         System.out.println("Данные ФИО сотрудника - " + fullName);
+
+        System.out.println("Дополнительное задание 5 (4)");
+        fullName = "Ivanov Ivan Ivanovich";
+        lastName = fullName.substring(0,fullName.indexOf(" "));
+        firstName = fullName.substring(fullName.indexOf(" ") + 1,fullName.lastIndexOf(" "));
+        middleName = fullName.substring(fullName.lastIndexOf(" ") + 1,fullName.length());
+        System.out.println("Фамилия сотрудника - " + lastName);
+        System.out.println("Имя сотрудника - " + firstName);
+        System.out.println("Отчество сотрудника - " + middleName);
+
+        System.out.println("Дополнительное задание 6 (5)");
+        fullName = "ivanov ivan ivanovich";
+        String[] fullNameSplit = fullName.split(" ");
+        char[][] fullNameSplitAndChar = {fullNameSplit[0].toCharArray(), fullNameSplit[1].toCharArray(), fullNameSplit[2].toCharArray()};
+        fullNameSplitAndChar[0][0] = Character.toUpperCase(fullNameSplitAndChar[0][0]);
+        fullNameSplitAndChar[1][0] = Character.toUpperCase(fullNameSplitAndChar[1][0]);
+        fullNameSplitAndChar[2][0] = Character.toUpperCase(fullNameSplitAndChar[2][0]);
+        fullNameSplit[0] = new String(fullNameSplitAndChar[0]);
+        fullNameSplit[1] = new String(fullNameSplitAndChar[1]);
+        fullNameSplit[2] = new String(fullNameSplitAndChar[2]);
+        fullName = fullNameSplit[0] + " " + fullNameSplit[1] + " " + fullNameSplit[2];
+        System.out.println("Верное написание ФИО сотрудника с заглавных букв - " + fullName);
+
+        System.out.println("Дополнительное задание 7 (6)");
+        String stringA = "135";
+        String stringB = "246";
+        char[] stringAChar = stringA.toCharArray();
+        char[] stringBChar = stringB.toCharArray();
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < stringA.length(); i++) {
+            result.append(stringAChar[i]);
+            result.append(stringBChar[i]);
+        }
+        System.out.println("Данные строки - " + result);
+
+        System.out.println("Дополнительное задание 8 (7)");
+        String alphabet = "aabccddefgghiijjkk";
+        StringBuilder doubles = new StringBuilder();
+        char[] alphabetChar = alphabet.toCharArray();
+        for (int i = 0; i < alphabetChar.length - 1; i++) {
+            if (alphabetChar[i] == alphabetChar[i + 1]) {
+                doubles.append(alphabetChar[i]);
+            }
+        }
+        System.out.println(doubles);
     }
 }
